4d397071fbf48e85e68037a0d2a3dd993a52ef02
Oscar <yuszy@outlook.com>
2016-02-20 20:15:35 -0800

    Added "two sum" solution

--- Java_solution/src/BurstBalloons.java
+++ Java_solution/src/BurstBalloons.java
@@ -1,34 +1,37 @@
-public class Solution
+/**
+ * Created by oscar on 2/20/16.
+ */
+public class BurstBalloons
 {
     public int maxCoins(int[] nums)
     {
         int n = nums.length;
         int[][] dp = new int[n + 2][n + 2];
         int[] newNums = new int[n + 2];
-        
+
         for (int i = 0; i < n; i++)
         {
             newNums[i + 1] = nums[i];
         }
-        
+
         newNums[0] = newNums[n + 1] = 1;
-        
+
         return divide(1, n, newNums, dp);
     }
-    
+
     public int divide(int i, int j, int[] nums, int[][] dp)
     {
         if (dp[i][j] > 0)
         {
             return dp[i][j];
         }
-        
+
         for (int x = i; x <= j; x++)
         {
-            dp[i][j] = Math.max(dp[i][j], 
-                divide(i, x - 1, nums, dp) + nums[i - 1] * nums[x] * nums[j + 1] + divide(x + 1, j, nums, dp));
+            dp[i][j] = Math.max(dp[i][j],
+                    divide(i, x - 1, nums, dp) + nums[i - 1] * nums[x] * nums[j + 1] + divide(x + 1, j, nums, dp));
         }
-        
+
         return dp[i][j];
     }
 }
