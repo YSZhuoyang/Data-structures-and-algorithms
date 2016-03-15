fd18bb7fbaf6090aa269b0efb3ef80bd51e7c148
Oscar <yuszy@outlook.com>
2016-03-16 09:11:29 +1100

    Added my second solution and a recommended solution

--- Java_solution/src/RemoveDuplicatesFromSortedArray.java
+++ Java_solution/src/RemoveDuplicatesFromSortedArray.java
@@ -3,7 +3,7 @@
  */
 public class RemoveDuplicatesFromSortedArray
 {
-    // My solution
+    // My solution no.1
     public int removeDuplicates(int[] nums)
     {
         if (nums == null || nums.length < 1)
@@ -61,4 +61,69 @@ public class RemoveDuplicatesFromSortedArray
 
         return count;
     }
+
+    // My solution no.2
+    /*public int removeDuplicates(int[] nums)
+    {
+        if (nums == null || nums.length < 1)
+        {
+            return 0;
+        }
+        else if (nums.length == 1)
+        {
+            return 1;
+        }
+
+        int count = 1;
+        int left = 0;
+        int right;
+
+        // Find all duplicates and assign them with temp var
+        while (left < nums.length)
+        {
+            right = left + 1;
+
+            while (right < nums.length && nums[left] >= nums[right])
+            {
+                right++;
+            }
+            //System.out.println(right);
+
+            left++;
+
+            if (right - left == 0)
+            {
+                count++;
+            }
+            else if (right - left > 0 && right < nums.length)
+            {
+                nums[left] = nums[right];
+
+                count++;
+            }
+
+            if (right >= nums.length)
+            {
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    // A very good solution as reference
+    public int removeDuplicates(int[] nums)
+    {
+        int cur = 0;
+
+        for (int n : nums)
+        {
+            if(n>nums[cur])
+            {
+                nums[++cur] = n;
+            }
+        }
+
+        return cur + 1;
+    }*/
 }
