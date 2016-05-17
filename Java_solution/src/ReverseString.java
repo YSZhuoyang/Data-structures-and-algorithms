01656d1cac5da0dd4835a9d2b6c31d6bd9fd3e3a
Oscar <yuszy@outlook.com>
2016-05-17 18:51:29 +1000

    Added a new reverse solution

--- Java_solution/src/ReverseString.java
+++ Java_solution/src/ReverseString.java
@@ -21,4 +21,30 @@ public class ReverseString
 
 		return sb.toString();
 	}
+
+	// A faster recommended solution
+	/*public String reverseString(String s)
+	{
+		if (s == null || s.isEmpty() || s.length() == 1)
+		{
+			return s;
+		}
+
+		char[] cArray = new char[s.length()];
+
+		int head = 0;
+		int tail = s.length() - 1;
+		int middle = s.length() / 2;
+
+		while (head <= middle)
+		{
+			cArray[head] = s.charAt(tail);
+			cArray[tail] = s.charAt(head);
+
+			head++;
+			tail--;
+		}
+
+		return new String(cArray);
+	}*/
 }
