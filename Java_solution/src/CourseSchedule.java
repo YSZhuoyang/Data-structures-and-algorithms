a8954af6302cdacc1ed691bfa2e87d1aebeb1e34
Oscar <yuszy@outlook.com>
2016-07-04 20:37:02 +1000

    Optimized course schedule solutions with the ideas of DFS and BFS graph searching algorithms

--- Java_solution/src/CourseSchedule.java
+++ Java_solution/src/CourseSchedule.java
@@ -1,13 +1,25 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by oscar on 7/3/16.
  *
- * Idea: detect whether a graph contains a cycle
+ * Two approaches (both with a time complexity of O(V + E)):
+ * 1. Detect whether a graph contains a cycle using DFS
+ * 2. Find out whether all nodes are reachable using BFS
+ *
+ * Online course of graph searching: https://www.youtube.com/watch?v=AfSk24UTFS8
  */
 public class CourseSchedule
 {
 
+	// My DFS solution which beats 91% java submissions
+	// Faster than BFS. DFS is more suitable for detecting cycles
+	// within a graph.
+	//
+	// A good tutorial of detecting cycle in a graph using DFS:
+	// https://www.youtube.com/watch?v=rKQaZuoUR4M
 	public boolean canFinish(int numCourses, int[][] prerequisites)
 	{
 		if (prerequisites == null || numCourses < 0)
@@ -19,48 +31,137 @@ public class CourseSchedule
 			return true;
 		}
 
-		// Count number of prerequisites for each course
-		int[] numPres = new int[numCourses];
+		// Avoid duplicated edges and only check edges connected
+		// during DFS instead of trying all edges
+		List<Integer>[] prerequisiteLists = new ArrayList[numCourses];
+
+		for (int i = 0; i < numCourses; i++)
+		{
+		    prerequisiteLists[i] = new ArrayList<>();
+		}
 
 		for (int[] pair : prerequisites)
 		{
-			numPres[pair[0]]++;
+		    prerequisiteLists[pair[0]].add(pair[1]);
+		}
+
+		// Initialize a visited set and a visiting set
+		boolean[] visiting = new boolean[numCourses];
+		boolean[] visited = new boolean[numCourses];
+
+		// Do a DFS for each root node
+		for (int i = 0; i < numCourses; i++)
+		{
+			if (!dfs(visiting, visited, prerequisiteLists, i))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private boolean dfs(boolean[] vSet, boolean[] fSet, List<Integer>[] prerequisiteLists, int currNode)
+	{
+		// Current node is not in the visited set
+		if (!fSet[currNode])
+		{
+			// A cycle is detected if the current node being visited
+			// is already in the visiting set.
+			if (vSet[currNode])
+			{
+				return false;
+			}
+			// Add the current node into the visiting set and keep going
+			// through its children
+			else
+			{
+				vSet[currNode] = true;
+
+				for (int i : prerequisiteLists[currNode])
+				{
+					if (!dfs(vSet, fSet, prerequisiteLists, i))
+					{
+						return false;
+					}
+				}
+
+				fSet[currNode] = true;
+				vSet[currNode] = false;
+
+				return true;
+			}
+		}
+		// Current node is already in the finished set, which means the
+		// whole subtree starting from that node has been entirely visited
+		else
+		{
+			return true;
+		}
+	}
+
+	// My BFS solution which beats 80% java submissions.
+	// BFS is more suitable for finding a shortest path between
+	// the root node and another node within a graph.
+	/*public boolean canFinish(int numCourses, int[][] prerequisites)
+	{
+		if (prerequisites == null || numCourses < 0)
+		{
+			return false;
+		}
+		else if (prerequisites.length == 0 || numCourses == 0)
+		{
+			return true;
+		}
+
+		// Only check edges connected during DFS instead of
+		// trying all edges
+		List<Integer>[] successorLists = new ArrayList[numCourses];
+		// Count number of prerequisites for each course
+		int[] numPres = new int[numCourses];
+
+		for (int i = 0; i < numCourses; i++)
+		{
+		    successorLists[i] = new ArrayList<>();
+		}
+
+		for (int[] pair : prerequisites)
+		{
+		    successorLists[pair[1]].add(pair[0]);
+		    numPres[pair[0]]++;
 		}
 
 		// Store nodes with no prerequisite into the queue
 		// as start nodes
 		ArrayDeque<Integer> q = new ArrayDeque<>();
+		int numReachable = 0;
 
 		for (int i = 0; i < numCourses; i++)
 		{
 			if (numPres[i] == 0)
 			{
 				q.add(i);
+				numReachable++;
 			}
 		}
-
-		int numStart = q.size();
 
 		// Do a bfs
 		while (!q.isEmpty())
 		{
 			int currNode = q.remove();
 
-			for (int[] pair : prerequisites)
+			for (int i : successorLists[currNode])
 			{
-				if (pair[1] == currNode)
-				{
-					numPres[pair[0]]--;
+			    numPres[i]--;
 
-					if (numPres[pair[0]] == 0)
-					{
-						q.add(pair[0]);
-						numStart++;
-					}
+				if (numPres[i] == 0)
+				{
+					q.add(i);
+					numReachable++;
 				}
 			}
 		}
 
-		return numStart == numCourses;
-	}
+		return numReachable == numCourses;
+	}*/
 }
