0a52123b695450209da7296dc7bc34a1a24c5b97
OscarYu <yuszy@outlook.com>
2017-03-11 23:31:46 +1100

    Update PathSum.java

--- Java_solution/src/PathSum.java
+++ Java_solution/src/PathSum.java
@@ -7,19 +7,19 @@ public class PathSum
     // My solution using DFT
     public boolean hasPathSum(TreeNode root, int sum)
     {
-        return depthTraversal(root, 0, sum);
+        if (root == null) return false;
+        
+        return dfs(root, 0, sum);
     }
 
     private boolean depthTraversal(TreeNode node, int sum, int target)
     {
-        if (node != null && (node.left != null || node.right != null))
-        {
-            return depthTraversal(node.left, sum + node.val, target) ||
-                    depthTraversal(node.right, sum + node.val, target);
-        }
-        else
-        {
-            return node != null && node.val + sum == target;
-        }
+        if (n.left == null && n.right == null)
+            return v + n.val == s;
+        
+        v += n.val;
+        
+        return n.left != null && dfs(n.left, v, s) ||
+            n.right != null && dfs(n.right, v, s);
     }
 }
