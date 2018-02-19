ec0e752a360180215f84bd28eacbbe256ddd4067
oscar <yuszy@outlook.com>
2018-02-19 23:58:48 +1100

    Added topological sort solution

--- Go_solution/MinCostClimbingStairs.go
+++ Go_solution/MinCostClimbingStairs.go
@@ -18,6 +18,8 @@ func minCostClimbingStairs(cost []int) int {
 	return climb(l, cost, costMemo)
 }
 
+// Solution 1: Linear space, linear time
+// By top down recursion
 func climb(i int, cost []int, costMemo map[int]int) int {
 	if i < 2 {
 		return 0
@@ -37,7 +39,33 @@ func climb(i int, cost []int, costMemo map[int]int) int {
 
 	if p1 > p2 {
 		return p2
-	} else {
-		return p1
 	}
+
+	return p1
+}
+
+// Solution 2: constant space, linear time
+// By bottom up topological sort
+func climb2(cost []int) int {
+	var memo [2]int
+	memo[0] = 0
+	memo[1] = 0
+	l := len(cost)
+	for i := 1; i < l; i++ {
+		// Idea:
+		// TotalCost1(n) = TotalCost(n - 1) + Cost(n - 1)
+		// TotalCost2(n) = TotalCost(n - 2) + Cost(n - 2)
+		// TotalCost(n) = min(TotalCost1(n), TotalCost2(n))
+
+		t1 := memo[1] + cost[i]
+		t2 := memo[0] + cost[i-1]
+		memo[0] = memo[1]
+		if t1 < t2 {
+			memo[1] = t1
+		} else {
+			memo[1] = t2
+		}
+	}
+
+	return memo[1]
 }
