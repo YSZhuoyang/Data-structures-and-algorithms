5ff489b9f769715c219592e43eac13a31b2f433a
Oscar Yu <yuszy@outlook.com>
2019-11-02 17:18:58 +1100

    updated multi threaded search server files solution

--- Py_solution/searchServerFiles.py
+++ Py_solution/searchServerFiles.py
@@ -1,6 +1,6 @@
 #
-# Given an interface: getFilesFolders(absPath, queryStr) which makes a request to a server
-# and receives a response containing all files and folders under the given path,
+# Given an interface: getFilesFolders(absPath) which makes a request to a server
+# and receives a response containing all files and folders under the dir of given path,
 # implement a method: searchFile(absPath, queryStr) which can return a file matched by
 # the given query string within the directory & sub directories of given path.
 #
@@ -13,12 +13,11 @@
 #
 # Note: be careful of the output format
 #
-# Followup: change getFilesFolders(...) to be async and non-blocking and called with a 3rd
-#           param 'callback'
+# Followup: change getFilesFolders(...) to be async and non-blocking and called with a 'callback'
 #
 # Idea:
 # 1. visualize the tree of directories
-# 2. search for file recursively in the tree
+# 2. DFS / BFS search
 #
 # Time: O(D + F * LP * LQ) where:
 #   D is total number of directories
@@ -29,12 +28,12 @@ import asyncio
 
 
 # Stub method returning folders and files under /root dir
-def getFilesFolders(absPath, query):
+def getFilesFolders(absPath):
     return [''], ['file']
 
 
 def searchFile(absPath, query):
-    files, folders = getFilesFolders(absPath, query)
+    files, folders = getFilesFolders(absPath)
     for f in files:
         absFilePath = absPath + '/' + f
         if query in absFilePath:
@@ -56,45 +55,76 @@ print(searchFile('/root', 't/f'))
 # Time: O(log(D)) since code at the same level of the tree can be executed
 #       concurrently, the depth of the tree has the upper bound of O(log(D))
 
-# Work around async - await with counting
-callCount = 0
-respCount = 0
-
-
-# Stub method
-def getFilesFoldersAsync(absPath, query, callback):
-    callback([''], [''])
+from threading import Thread, Lock
 
 
 def searchFileAsync(absPath, query):
-    res = None
-    callCount += 1
+    # Note that a thread cannot write to local vars in another thread,
+    # but it can be done through references
+    res = set()
+    queue = [absPath]
+    currDepth = [1]
+    maxDepth = 3
+    # Avoid thread conflicts when manipulating result set and queue
+    appQueueLock = Lock()
+    mergeResLock = Lock()
 
-    def callback(files, folders):
+    def searchDirFunc(absPath, depth):
+        currDirs, currFiles, currDepth[0] = getFilesFoldersFunc(absPath, depth)
+        p = searchFunc(currFiles, currDirs, absPath)
+        if p:
+            with mergeResLock:
+                res.add(p)
+
+    def getFilesFoldersFunc(absPath, currDepth):
+        # Stub download method
+        if currDepth[0] > maxDepth:
+            return [], [], currDepth[0]
+
+        return ['curr', 'curr2'], ['file1', 'file2'], currDepth[0] + 1
+
+    def searchFunc(files, folders, absPath):
         for f in files:
             absFilePath = absPath + '/' + f
             if query in absFilePath:
-                res = absFilePath
-                respCount += 1
-                return
+                return absFilePath
 
-        for d in folders:
-            absDirPath = absPath + '/' + d
-            searchFileAsync(absDirPath, query)
+        with appQueueLock:
+            for d in folders:
+                absDirPath = absPath + '/' + d
+                queue.append(absDirPath)
 
-        respCount += 1
+        return None
 
-    getFilesFoldersAsync(absPath, query, callback)
+    while queue:
+        tasks = []
+        for currPath in queue:
+            task = Thread(target=searchDirFunc, args=(currPath, currDepth))
+            tasks.append(task)
+        queue.clear()
 
-    while callCount > respCount:
-        continue
+        for task in tasks:
+            task.start()
 
-    return res
+        for task in tasks:
+            task.join()
+            if len(res) > 0:
+                return list(res)[0]
+
+    return None
+
+
+print(searchFileAsync('/root', 'ot/curr/file1'))
 
 
 # Solution 2 with await
+# Stub method
+async def getFilesFoldersAsync(absPath):
+    return ([''], [''])
+
+
 async def searchFileAsyncII(absPath, query):
-    files, folders = await getFilesFoldersAsync(absPath, query)
+    files, folders = await getFilesFoldersAsync(absPath)
     for f in files:
         absFilePath = absPath + '/' + f
         if query in absFilePath:
