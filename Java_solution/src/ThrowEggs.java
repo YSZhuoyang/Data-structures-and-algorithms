d1fd9a129066a820bfd81907ba54624c4bfe5799
Oscar Yu <yuszy@outlook.com>
2020-03-15 17:26:06 +1100

    reimplemented throw eggs solution

--- Java_solution/src/ThrowEggs.java
+++ Java_solution/src/ThrowEggs.java
@@ -1,57 +1,85 @@
 /**
- * Created by oscar on 6/29/16.
- *
  * Given k eggs, a building with n floors, find out the
  * highest floor where an egg being dropped will not break
  * (an unrealistic problem but a good practice for binary search)
  * eggs can be picked up if they do not break.
+ *
+ * Constraints:
+ * 1 <= K <= 100
+ * 1 <= N <= 10000
  */
-public class ThrowEggs
-{
+class Solution {
+	// class Key {
+    //     public Integer x;
+    //     public Integer k;
+        
+    //     public Key(int x, int k) {
+    //         this.x = x;
+    //         this.k = k;
+    //     }
 
-	public int findFloor(int k, int n)
-	{
-		if (k < 1 || n < 1)
-		{
-			return -1;
-		}
-		else if (!eggBreak(n))
-		{
-			return n;
-		}
+    //     @Override
+    //     public int hashCode() {
+    //         return x.hashCode() ^ k.hashCode();
+    //     }
 
-		return binarySearch(1, n, k);
-	}
+    //     @Override
+    //     public boolean equals(Object o) {
+    //         Key other = (Key)o;
+    //         return x == other.x && k == other.k;
+    //     }
+    // }
 
-	public int binarySearch(int left, int right, int k)
-	{
-		if (k == 1)
-		{
-			for (int i = left; i < right; i++)
-			{
-				if (eggBreak(i))
-				{
-					return i;
-				}
-			}
+    private HashMap<Integer, Integer> memo = new HashMap<>();
 
-			return right;
-		}
+    public int superEggDrop(int K, int N) {
+        // Idea:
+        // 1. Divide and conquer with substructure (let function numThrow takes 2 params: N floors, K eggs
+        //    and return mininum number of steps):
+        //      numThrow(N, K) = min( max( numThrow(X - 1, K - 1), numThrow(N - X, K - 1) ) | for all X in [1, N] )
+        //      numThrow(N, 1) = N
+        //
+        // 2. Memorization
+        // 3. Bsearch
+        //
+        // Time: O(N * K * log(N))
 
-		int middle = (left + right) / 2;
+        // Start throwing from the 1st floor until Fth floor is found if there's only 1 egg left
+        if (K == 1) return N;
+        else if (N == 1) return 1;
+        else if (N == 0) return 0;
 
-		if (!eggBreak(middle))
-		{
-			return binarySearch(middle, right, k - 1);
-		}
-		else
-		{
-			return binarySearch(left, middle, k - 1);
-		}
-	}
+		// Using a Tuple class object as key is a lot slower than using an integer based on given constraints
+        int key = K * 100000 + N;
+        if (memo.containsKey(key)) return memo.get(key);
 
-	public boolean eggBreak(int l)
-	{
-		return l > 5;
-	}
+        int minThrow = N;
+        // Bsearch for min of max(F(K - 1, X - 1), F(K, N - X)) based on the fact:
+		//     F(K - 1, X - 1) monotonically increases
+		//     F(K, N - X) monotonically decreases with respect to X
+		// thus:
+		//     the part on the left of cross point satisfies condition: F(K - 1, X - 1) < F(K, N - X)
+		//     the part on the right of cross point satisfies condition: F(K - 1, X - 1) > F(K, N - X)
+        int l = 1;
+        int r = N;
+        while (l < r) {
+            int x = (int) ((l + r) / 2);
+            // Throw an egg at the Xth floor
+            // Egg broke thus F < x
+            int numThrowBeforeX = superEggDrop(K - 1, x - 1); // Monotonically inc
+            // Egg did not broke thus F >= x
+            int numThrowAfterX = superEggDrop(K, N - x);      // Monotonically dec
+            // Pick the worst case given F can be any floor
+            int numThrow = Math.max(numThrowBeforeX, numThrowAfterX) + 1;
+            minThrow = Math.min(minThrow, numThrow);
+            if (numThrowBeforeX < numThrowAfterX)
+                l = x + 1; // Go right
+            else
+                r = x; // Go left;
+        }
+
+        memo.put(key, minThrow);
+
+        return minThrow;
+    }
 }
